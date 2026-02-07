db8150a97f284821132a4d7fbfd827867d365421
lamliliann <lamlilian365@gmail.com>
2026-02-07 12:10:10 -0500

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class MAIN{  
+  public static void main(String[] args){
+
+    System.out.println("Guess a number between 1 and 100");
+        Scanner scan = new Scanner(System.in);
+        int guessNumber = 0;
+        int hiddenNumber = 11;
+       // int attempts = 0;
+        
+        while (guessNumber!= hiddenNumber){
+            guessNumber= scan.nextInt();
+          //  attempts++;
+         if(guessNumber < hiddenNumber){
+          System.out.println("Too low");
+        }
+        else if (guessNumber> hiddenNumber){
+          System.out.println("Too high");
+        }
+        else{
+          System.out.println("You got it!");
+         // System.out.println("The amount of guess you took were: " + attempts);
+        }
+      }
+      scan.close();
+
+  }
+}
